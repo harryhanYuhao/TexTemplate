c76f037511fd27e7889b4c4c28e7d20ffc3f4115
harryhanYuhao <s2162783@ed.ac.uk>
2023-05-07 11:46:17 +0100

    update installation script & macros

--- math/install.sh
+++ math/install.sh
@@ -1,11 +1,30 @@
-#!bin/sh
+#!/bin/sh
 
-mkdir ~/psu/bin/tex
-cp main.tex preamble.tex -t ~/psu/bin/tex
+RED="\033[0;31m"
+NC="\033[0m"
 
-cp .texinit -t ~/psu/bin/tex
-mv ~/psu/bin/tex/texinit ~/psu/bin/tex/texinit
-sudo chmod +x ~/psu/bin/tex/texinit
+mkdir -p $HOME/psu/bin/tex
+cp main.tex preamble.tex macros.tex -t $HOME/psu/bin/tex
 
-cp ~/.bashrc ~/.bashrc.bak
-echo "PATH=\$PATH:~/psu/bin/tex" >> ~/.bashrc
+mkdir -p $HOME/psu/bin/tex
+cp texinit -t $HOME/psu/bin/tex
+chmod +x $HOME/psu/bin/tex/texinit
+
+printf "Appending To Path\n"
+if [ -f $HOME/.bashrc.bak ]; then
+	printf "${HOME}/.bashrc.bak exists, do you want to remove it? (y/n)"
+	read response
+	if [ "$response" = "y" ]; then
+		rm -rf $HOME/.bashrc.bak
+		printf "$HOME/.bashrc.bak ${RED}deleted${NC}\n"
+	else
+		printf "${RED}FAILED${NC} to back up old configuration!\n"
+		printf "Installation ${RED}aborted${NC}\n"
+		exit 1
+	fi
+fi
+
+echo "Backing up old config"
+echo "cp $HOME/.bashrc $HOME/.bashrc.bak"
+cp $HOME/.bashrc $HOME/.bashrc.bak
+echo "PATH=\$PATH:$HOME/psu/bin/tex" >> $HOME/.bashrc
